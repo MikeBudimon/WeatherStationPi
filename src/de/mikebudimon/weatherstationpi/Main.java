835ac20b78efa98cacd4851a90741e382bd0ad35
Michal Wagner <mikebudimon@gmail.com>
2016-01-08 03:21:43 +0100

    - optimized and updated code for better functionality.

--- src/de/mikebudimon/weatherstationpi/Main.java
+++ src/de/mikebudimon/weatherstationpi/Main.java
@@ -15,7 +15,6 @@ public class Main {
             //System.out.println("Receiving sensordata...");
             float[] sData = sensorData.getData();
 
-
             //System.out.println("Connecting to a selected database...");
             connectDB.useDatabase();
             //System.out.println("Successfully connected to database!");
